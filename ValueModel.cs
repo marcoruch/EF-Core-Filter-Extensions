3a54881f2e52c7595d2b3c33ba99da4c535e21ff
Marco Ruch <marco_ruch@gmx.ch>
2020-07-31 10:19:56 +0200

    Create ValueModel.cs

---
+++ ValueModel.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Shared.Models.Filter
+{
+    public class ValueModel
+    {
+        public object Value { get; set; }
+        public object Value2 { get; set; }
+    }
+}
