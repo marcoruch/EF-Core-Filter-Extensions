49508241aba65a6d5d2b53cf6bc72daf4d034f90
Marco Ruch <marco_ruch@gmx.ch>
2020-07-31 10:18:37 +0200

    Create FilterModel.cs

---
+++ FilterModel.cs
@@ -0,0 +1,22 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Shared.Models.Filter
+{
+    public class FilterModel<TFilterType>
+    {
+        public TFilterType FilterType { get; set; }
+        /// <summary>
+        /// Model used to pass Values, when the Filter is not a "Between" Filter, the first Value will be used.
+        /// </summary>
+        public ValueModel Value { get; set; }
+        public string Name { get; set; }
+
+        public FilterModel(TFilterType filterType, ValueModel value)
+        {
+            FilterType = filterType;
+            Value = value;
+        }
+    }
+}
